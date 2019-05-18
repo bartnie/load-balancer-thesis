1e6999eaeecf274244d45c38b472454be48ae41f
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2019-05-19 01:01:36 +0200

    controllers added in worker module

---
+++ worker/src/main/java/pl/bartek/thesis/worker/controller/ConfigurationController.java
@@ -0,0 +1,43 @@
+package pl.bartek.thesis.worker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.bartek.thesis.worker.configuration.CurrentWorkerConfiguration;
+import pl.bartek.thesis.worker.dto.configuration.WorkerConfigurationDto;
+
+import javax.validation.Valid;
+
+@RestController("/configuration")
+public class ConfigurationController {
+    private static final Logger LOG = LoggerFactory.getLogger(ConfigurationController.class);
+
+    private final CurrentWorkerConfiguration currentConfiguration;
+
+    @Autowired
+    public ConfigurationController(final CurrentWorkerConfiguration currentConfiguration) {
+        this.currentConfiguration = currentConfiguration;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> setConfiguration(@Valid @RequestBody final WorkerConfigurationDto configurationDto) {
+        LOG.info("Recieved configuration: " + configurationDto);
+        currentConfiguration.changeConfiguration(configurationDto);
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping
+    public ResponseEntity<WorkerConfigurationDto> setConfiguration() {
+        final WorkerConfigurationDto currentConfigurationDto = currentConfiguration.gatCurrentConfigurationDto();
+        LOG.info("Recieved configuration: " + currentConfigurationDto);
+
+        return new ResponseEntity<>(currentConfigurationDto, HttpStatus.ACCEPTED);
+    }
+}
---
+++ worker/src/main/java/pl/bartek/thesis/worker/controller/DoWorkController.java
@@ -0,0 +1,30 @@
+package pl.bartek.thesis.worker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.bartek.thesis.worker.service.work.WorkService;
+
+@RestController
+public class DoWorkController {
+    private static final Logger LOG = LoggerFactory.getLogger(DoWorkController.class);
+
+    private final WorkService workService;
+
+    @Autowired
+    public DoWorkController(final WorkService workService) {
+        this.workService = workService;
+    }
+
+    @GetMapping("/work")
+    public ResponseEntity<String> doWork(){
+        LOG.info("Work request received");
+        workService.doWork();
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+}
