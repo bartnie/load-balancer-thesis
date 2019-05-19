3a0a4d0d5a8c0bf840ad263d07f89ecb6528db36
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2019-05-19 16:39:58 +0200

    configuration controller changed to use new worker cache object

--- master/src/main/java/pl/bartek/thesis/master/controller/ConfigurationController.java
+++ master/src/main/java/pl/bartek/thesis/master/controller/ConfigurationController.java
@@ -9,30 +9,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.bartek.thesis.master.cache.configuration.WorkerConfigurationCache;
 import pl.bartek.thesis.master.domain.test.specification.TestSpecification;
 import pl.bartek.thesis.master.domain.worker.configuraton.WorkerConfiguration;
 import pl.bartek.thesis.master.service.configuration.ConfigurationService;
 import pl.bartek.thesis.master.service.test.TestService;
 
-import static pl.bartek.thesis.master.cache.configuration.WorkerConfigurationCache.CACHE;
-
 @Controller
 public class ConfigurationController {
     private static final Logger LOG = LoggerFactory.getLogger(ConfigurationController.class);
 
+    private final WorkerConfigurationCache cache;
     private final ConfigurationService configurationService;
     private final TestService testService;
 
     @Autowired
-    public ConfigurationController(final ConfigurationService configurationService, final TestService testService) {
+    public ConfigurationController(final WorkerConfigurationCache cache, final ConfigurationService configurationService, final TestService testService) {
+        this.cache = cache;
         this.configurationService = configurationService;
         this.testService = testService;
     }
 
     @GetMapping("/")
     public String getConfigurePage(final Model model) {
-        model.addAttribute("worker1", CACHE.get("127.0.0.1"));
-        model.addAttribute("worker2", CACHE.get("255.127.0.1"));
+        model.addAttribute("workers", cache.getConfigurationsList());
+        model.addAttribute("newWorker", new WorkerConfiguration());
         model.addAttribute("testSpecification", new TestSpecification());
         return "configurePage";
     }
@@ -40,19 +41,19 @@ public class ConfigurationController {
     @PostMapping("/send")
     public String sendConfiguration(@ModelAttribute("worker") final WorkerConfiguration configuration,
                                     final RedirectAttributes redirectAttributes) {
-        CACHE.put(configuration.getIp(), configuration);
-        LOG.info("SENDING CONFIGURATION " + configuration);
+        cache.updateConfiguration(configuration);
+        LOG.info("SENDING configuration " + configuration);
 
         configurationService.sendConfiguration(configuration);
         redirectAttributes.addFlashAttribute("message",
-                String.format("Configuration sent succesfully to worker service with IP: %s!", configuration.getIp()));
+                String.format("Configuration sent succesfully to worker service with URI: %s!", configuration.getInstanceURI()));
         return "redirect:/";
     }
 
     @PostMapping("/test")
     public String testLoadBalancer(@ModelAttribute("testSpecification") final TestSpecification testSpecification,
                                    final RedirectAttributes redirectAttributes) {
-        LOG.info("RUN TEST " + testSpecification);
+        LOG.info("RUNING test " + testSpecification);
 
         testService.testLoadBalancer(testSpecification);
         redirectAttributes.addFlashAttribute("message", "Test ran succesfully!");
