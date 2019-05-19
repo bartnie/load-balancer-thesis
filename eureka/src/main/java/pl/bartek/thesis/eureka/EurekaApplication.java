fc7425791f22f6dc5311219f212ddca031e2b402
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2019-05-19 16:29:13 +0200

    eureka server enabled, master and worker services enabled as discovery server clients

--- eureka/src/main/java/pl/bartek/thesis/eureka/EurekaApplication.java
+++ eureka/src/main/java/pl/bartek/thesis/eureka/EurekaApplication.java
@@ -2,11 +2,14 @@ package pl.bartek.thesis.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
 
--- worker/src/main/java/pl/bartek/thesis/worker/WorkerApplication.java
+++ worker/src/main/java/pl/bartek/thesis/worker/WorkerApplication.java
@@ -2,11 +2,13 @@ package pl.bartek.thesis.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WorkerApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(WorkerApplication.class, args);
     }
 
